78dd735c7bcd2f950f306f6c7011aa2675237654
栗田大輔 <daikons.msn36@gmail.com>
2022-02-14 01:36:22 +0900

    グループ管理者の権限の実装

--- app/controllers/groupings_controller.rb
+++ app/controllers/groupings_controller.rb
@@ -1,49 +1,68 @@
 class GroupingsController < ApplicationController
+  before_action :set_grouping, only: %i[ update destroy ]
   before_action :authenticate_user!
 
   def create
     email_exist? and return
     user_exist? and return
-    group = find_group(params[:group_id])
-    user = User.find_by(email: grouping_params) 
+    group = find_group(params_grouo_id)
+    user = User.find_by(email: params_email) 
     if user
       group.groupings.create(user_id: user.id)
-      redirect_to group_path(group.id), notice: t('notice.member_registration_completed', email: grouping_params)
+      redirect_to group_path(group.id), notice: t('notice.member_registration_completed', email: params_email)
+    else
+      redirect_to group_path(group.id)
     end
   end
   
-  def destroy
-    grouping = Grouping.find(params[:id])
-    grouping.destroy
-    redirect_to group_path(params[:group_id]), notice: t('notice.delete_member', email: grouping.user.email)
+  def update
+    if @grouping.update(admin: params[:admin])
+      redirect_to group_path(params_grouo_id)
+    else
+      redirect_to group_path(params_grouo_id)
+    end
   end
+
+  def destroy
+    @grouping.destroy
+    redirect_to group_path(params_grouo_id), notice: t('notice.delete_member', email: @grouping.user.email)
+  end
+
   
   private
   
-  def grouping_params
+  def params_email
     params[:email]
+  end
+
+  def params_grouo_id
+    params[:group_id]
   end
   
   # すでにメンバー登録されている場合はグループ詳細画面に遷移する
   # リファクタリング（モデルに持っていく）
   def email_exist?
-    group = find_group(params[:group_id])
-    redirect_to group_path(group.id), notice: t('notice.registered_as_a_member', email: grouping_params) if group.members.exists?(email: grouping_params)
+    group = find_group(params_grouo_id)
+    redirect_to group_path(group.id), notice: t('notice.registered_as_a_member', email: params_email) if group.members.exists?(email: params_email)
   end
   
   # ユーザとして存在しない場合はグループ詳細画面に遷移する
   # リファクタリング（モデルに持っていく）
   def user_exist?
-    group = find_group(params[:group_id])
-    if !grouping_params.present?
+    group = find_group(params_grouo_id)
+    if !params_email.present?
       redirect_to group_path(group.id), notice: t('notice.email_blank')
-    elsif !User.exists?(email: grouping_params)
-      redirect_to group_path(group.id), notice: t('notice.no_email_create_an_account', email: grouping_params)
+    elsif !User.exists?(email: params_email)
+      redirect_to group_path(group.id), notice: t('notice.no_email_create_an_account', email: params_email)
     end
   end
 
-  # Group.find(params[:group_id])の共通化
+  # Group.find(params_grouo_id)の共通化
   def find_group(_group_id)
-    Group.find(params[:group_id])
+    Group.find(params_grouo_id)
+  end
+
+  def set_grouping
+    @grouping = Grouping.find(params[:id])
   end
 end
