c6eed148894ab9addf31c1ad1096e51fea14c308
栗田大輔 <daikons.msn36@gmail.com>
2022-03-07 05:25:51 +0900

    メンバーが所属しているグループのみ操作・表示ができるよう実装しました

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,10 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
+  before_action :login_user_group?, only: %i[ show edit update destroy ]
 
-  def index
-    @groups = Group.select(:id, :name, :explanation)
+  def index # ログインユーザーのみのグループ一覧を表示する
+    @groups = current_user.groups
   end
   
   def show
@@ -47,5 +48,9 @@ class GroupsController < ApplicationController
 
   def set_group
     @group = Group.find(params[:id])
+  end
+
+  def login_user_group? # 所属していないグループにアクセスすると、グループ一覧にリダイレクトされる
+    redirect_to groups_path unless current_user.groups.ids.include?(params[:id].to_i)
   end
 end
