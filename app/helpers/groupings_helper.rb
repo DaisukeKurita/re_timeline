44f079480fee809e7e77ce486925e109161db5da
栗田大輔 <daikons.msn36@gmail.com>
2022-03-08 08:28:17 +0900

    groupings_controllerとgroupings_helperの挙動がわかりやすいように整理しました

--- app/helpers/groupings_helper.rb
+++ app/helpers/groupings_helper.rb
@@ -1,6 +1,6 @@
 module GroupingsHelper
-  def group_admin?
-    @groupings.try!(:each) do |grouping|
+  def group_admin? # ログイン中のユーザーがグループ管理者か判別を行なっている
+    @groupings.each do |grouping|
       return true if grouping.admin == true && current_user == grouping.user
     end
     false
