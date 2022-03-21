ee3b72d03c17792e27a3d666b41a1a6e1d342ea0
栗田大輔 <daikons.msn36@gmail.com>
2022-03-22 08:11:53 +0900

    グループ名は同じ名前が使われないよう実装をしました

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 255 }, uniqueness: true
   has_many :groupings, dependent: :destroy
   has_many :members, through: :groupings, source: :user
   has_many :blogs, dependent: :destroy
