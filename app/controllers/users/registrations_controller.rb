7fa7cbe3a02ac2bf3f317e6ca3393c7722a84d69
栗田大輔 <daikons.msn36@gmail.com>
2022-03-27 15:00:11 +0900

    アカウント情報を登録する前の確認画面を実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :ensure_normal_user, only: %i[update destroy]
   before_action :ensure_admin_user, only: %i[update destroy]
+  before_action :back_from_confirm, only: %i[create]
 
   def ensure_normal_user
     if resource.email == 'guest@example.com'
@@ -12,5 +13,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
     if resource.email == 'admin_guest@example.com'
       redirect_to root_path, notice: t('notice.admin_guest_user_cannot_be_updated_deleted')
     end
+  end
+  
+  def confirm
+    @user = User.new(sign_up_params)
+    if @user.valid?
+      render :confirm
+    else
+      @minimum_password_length = resource_class.password_length.min
+      render :new
+    end
+  end
+  
+  def back_from_confirm
+    @user = User.new(sign_up_params)
+    render :new and return if params[:back]
   end
 end
