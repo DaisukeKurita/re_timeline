a7aa25f90f0d64eb20f87a1a9e555ccc8e75c0bd
栗田大輔 <daikons.msn36@gmail.com>
2022-04-17 20:37:55 +0900

    メールの自動投稿機能を実装

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,22 +5,5 @@ class Group < ApplicationRecord
   has_many :blogs, dependent: :destroy
   has_many :maps, dependent: :destroy
 
-  enum receiving_date: { no_notice: 0, one_month_ago: 1, two_month_ago: 2, three_months_ago: 3 }
-  
-  def self.email_sending_settings
-    today = Date.today
-    one_week_later = today + 7
-    # return if today.mon == one_week_later.mon
-    case self.receiving_dates
-    when 1
-      puts "a"
-    when 2
-      puts "b"
-    when 3
-      puts "c"
-    else
-      puts "d"
-    end 
-    binding.pry
-  end
+  enum receiving_date: { no_notice: 0, one_month_ago: 1, two_months_ago: 2, three_months_ago: 3 }
 end
