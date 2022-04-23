fb6476bb283e569353d458f6160c02bf209fc8d5
栗田大輔 <daikons.msn36@gmail.com>
2022-04-23 16:24:31 +0900

    rakeファイルを作成しwheneverによる自動配信ができるよう修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,13 @@
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+rails_env = ENV['RAILS_ENV'] || :development
+env :PATH, ENV['PATH']
+set :environment, rails_env
+set :output, "#{Rails.root}/log/cron.log"
+
 every :friday, at: '7pm' do
-  runner "BlogsNoticeMailer.blogs_notice_mail"
+  rake 'scheduled_delivery:email_scheduled_delivery'
+end
+
+every 1.minute do
+  rake 'scheduled_delivery:email_scheduled_delivery'
 end
