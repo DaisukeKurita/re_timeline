09106dc7a3ddebce5fded5da1e13cefee7f97503
栗田大輔 <daikons.msn36@gmail.com>
2022-04-28 23:55:29 +0900

    Blog機能のテストコードを記述

--- spec/factories/blogs.rb
+++ spec/factories/blogs.rb
@@ -1,11 +1,47 @@
 FactoryBot.define do
   factory :blog do
-    new_contributor_id { 1 }
-    last_updater_id { 1 }
-    title { "MyString" }
-    content { "MyText" }
-    image { "MyText" }
-    event_date { "2022-03-15 05:43:01" }
-    notice { false }
+    last_updater_id { nil }
+    title { "title1" }
+    content { "content1" }
+    photo { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpeg')) }
+    event_date { "2022-03-15" }
+    email_notice { false }
+    association :new_contributor, factory: :user
+    association :group
+  end
+
+  factory :blog2, class: Blog  do
+    last_updater_id { nil }
+    title { "title2" }
+    content { "content2" }
+    photo { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpeg')) }
+    event_date { "2022-04-28" }
+    email_notice { false }
+    association :new_contributor, factory: :user
+    association :group
+  end
+
+  factory :blog3, class: Blog  do
+    new_contributor_id { nil }
+    last_updater_id { nil }
+    title { "title3" }
+    content { "content3" }
+    photo { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpeg')) }
+    event_date { "2015-12-28" }
+    email_notice { false }
+    association :new_contributor, factory: :user2
+    association :group2
+  end
+
+  factory :blog4, class: Blog  do
+    new_contributor_id { nil }
+    last_updater_id { nil }
+    title { "title4" }
+    content { "content4" }
+    photo { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpeg')) }
+    event_date { "2016-10-13" }
+    email_notice { false }
+    association :new_contributor, factory: :user2
+    association :group2
   end
 end
